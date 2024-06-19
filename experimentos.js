3e9dccc51bb1d17346c9eaf4c49404f7312bcc25
Santiago Trini <santiagotrini@gmail.com>
2024-06-18 22:40:56 -0300

    ejemplos

--- experimentos.js
+++ experimentos.js
@@ -18,3 +18,26 @@ setInterval(() => {
 newElement.classList.add('blue');
 
 title.style.textDecoration = 'underline'; // 
+
+
+// ciclo for ... of para loopear sobre una lista de elementos
+
+let board = document.createElement('div');
+board.classList.add('board');
+document.body.append(board);
+
+for (let i = 0; i < 49; i++) {
+  let square = document.createElement('div');
+  square.classList.add('square');
+  board.append(square);
+}
+
+let squares = document.querySelectorAll('.square');
+// aca tengo la lista de divs con clase square
+console.log(squares);
+
+let i = 0;
+
+for (let square of squares) {
+  if (i++ & 1) { square.classList.add('black'); };
+}
